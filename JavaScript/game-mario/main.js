6f76d8ea2c119fd692c6a14769e4da65b2b50d25
kumataiyo510 <kumataiyo510@yahoo.co.jp>
2021-01-13 17:09:19 +0900

    finished to 8 mario.html

--- JavaScript/game-mario/main.js
+++ JavaScript/game-mario/main.js
@@ -1,4 +1,4 @@
-// 次回（https://www.youtube.com/watch?v=YHW26_3e7-U）
+// 次回（https://www.youtube.com/watch?v=xshbtm3Qmyo）
 const GAME_FPS = 1000 / 60;
 const SCREEN_SIZE_W = 256;
 const SCREEN_SIZE_H = 224;
@@ -22,7 +22,6 @@ con.imageSmoothingEnabled       = false;
 con.webkitimageSmoothingEnabled = false;
 con.msimageSmoothingEnabled     = false;
 
-
 // フレームレート変数
 let frameCount = 0;
 let startTime;
@@ -34,80 +33,13 @@ chImg.onload = draw;
 // キーボード
 let keyb = {};
 
-// おじさん情報
-// 座標
-let oji_x  = 100<<4;
-let oji_y  = 100<<4;
-let oji_vx = 0;
-let oji_vy = 0;
-let oji_anime  =  0;
-let oji_sprite = 48;
-let oji_acount =  0;
-let oji_dir    =  0;
-let oji_jump   =  0;
-
-const ANIME_JUMP = 3;
+//おじさん作成
+let ojisan = new Ojisan(100, 100)
 
 // 更新処理
 function update(){
-    // アニメ用のカウンタ
-    oji_acount++;
-    if(Math.abs(oji_vx) == 32)oji_acount++;
-    
-    // ジャンプ
-    if(keyb.ABUTTON){
-        if(oji_jump == 0){
-            oji_anime = ANIME_JUMP;
-            oji_jump = 1;
-            oji_vy = -64;
-        }
-    }
-
-    // 重力
-    if(oji_vy < 64)oji_vy += 2;
-
-    //床にぶつかる
-    if(oji_y > 150<<3){
-        oji_jump = 0;
-        oji_vy = 0;
-        oji_y = 150<<3;
-    }
-    // 横移動
-    if(keyb.left){
-        if(oji_anime == 0)oji_acount = 0;
-        if(!oji_jump)oji_anime = 1;
-        oji_dir   = 1;
-        if(oji_vx > -32)oji_vx -= 1;
-        if(oji_vx > 0) oji_vx -= 1;
-        if(!oji_jump && oji_vx > 8)oji_anime = 2;
-    } else if(keyb.right){
-        if(oji_anime == 0)oji_acount = 0;
-        if(!oji_jump)oji_anime = 1;
-        oji_dir   = 0;
-        if(oji_vx <  32)oji_vx += 1;
-        if(oji_vx < 0) oji_vx += 1;
-        if(!oji_jump && oji_vx < -8)oji_anime = 2;
-    } else {
-        if(oji_vx > 0) oji_vx -= 1;
-        if(oji_vx < 0) oji_vx += 1;
-        if(!oji_jump && !oji_vx)    oji_anime = 0;
-    }
-
-
-    // スプライトの決定
-    if(oji_anime == 0) oji_sprite = 0;
-    else if(oji_anime == 1) oji_sprite = 2 + ((oji_acount/6)%3);    //アニメーションの繰り返し%3(2,3,4,2,3,4) /6はスピードを落としている
-    else if(oji_anime == 2) oji_sprite = 5;
-    else if(oji_anime == ANIME_JUMP) oji_sprite = 6;
-
-    // 左向きの時は＋４８を使う
-    if(oji_dir)oji_sprite += 48;
-
-    // 実際に座標を変えている
-    oji_x += oji_vx;
-    oji_y += oji_vy;
+    ojisan.update();
 }
-
 
 // スプライトの描画
 function drawSprite(snum, x, y){
@@ -124,7 +56,7 @@ function draw(){
     vcon.fillRect(0, 0, SCREEN_SIZE_W, SCREEN_SIZE_H);
 
     // おじさんを表示
-    drawSprite(oji_sprite, oji_x>>4, oji_y>>4);
+    ojisan.draw();
 
     // デバッグ情報を表示
     vcon.font = "24px 'Impact'";
@@ -133,7 +65,7 @@ function draw(){
     
     // 仮想画面から実画面へ拡大転送
     con.drawImage(vcan, 0, 0, SCREEN_SIZE_W, SCREEN_SIZE_H,
-                        0, 0, SCREEN_SIZE_W * 2, SCREEN_SIZE_H * 2)
+        0, 0, SCREEN_SIZE_W * 2, SCREEN_SIZE_H * 2)
 }
 
 // ゲームフレームレートの定義（60FPS）（*1 *2と選択的）
@@ -165,7 +97,6 @@ function mainLoop(){
     // 再帰でmainLoopを呼び出す（*2 *1と選択的）
     requestAnimationFrame(mainLoop);
 }
-
 
 // キーボードが押されたときに呼ばれる
 document.onkeydown = function(e){
